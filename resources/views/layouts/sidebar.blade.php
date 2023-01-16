87700da389929a03fdeeaeb438193409d2ccf914
Husnul M <husnulmub@gmail.com>
2023-01-16 15:10:31 +0700

    update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-6">
     <a href="/" class="brand-link">
-      <span class="brand-text font-weight-light">e-Procurement</span>
+      <span class="brand-text font-weight-light">Sales Visitor</span>
     </a>
 
     <div class="sidebar">
